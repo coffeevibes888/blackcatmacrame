a1888f49806c7aaf8cbc786b2e083bcbb203ef0b
Allen Young <vegaswarrior888@gmail.com>
2025-12-06 17:41:58 -0700

    updated to prisma 7 and NexyJS 16

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,7 +1,11 @@
 import { PrismaClient } from '@prisma/client';
+import { PrismaNeon } from '@prisma/adapter-neon';
 import { decryptField, encryptField } from '@/lib/encrypt';
 
-const prismaBase = new PrismaClient();
+const connectionString = process.env.DATABASE_URL as string;
+const adapter = new PrismaNeon({ connectionString });
+
+const prismaBase = new PrismaClient({ adapter });
 
 export const prisma = prismaBase
   .$extends({
